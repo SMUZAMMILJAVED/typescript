72f7d0a59182d9bf9c12c24ba32cbc008402baf7
S MuzammiL Javed <121771009+SMUZAMMILJAVED@users.noreply.github.com>
2025-09-11 14:06:32 +0500

    class_01 (TS)

---
+++ typescript_01/app.ts
@@ -0,0 +1,57 @@
+// var input_1=document.getElementById("num1")! as HTMLInputElement;
+// var input_2=document.getElementById("num2")! as HTMLInputElement;
+// var button=document.querySelector("button")!;
+// function add(a:number,b:number){
+//     return a+b
+// }
+// button.addEventListener("click",()=>{
+//     console.log(add(+input_1.value,+input_2.value));
+// })
+
+// number 5,6.67 ,-90
+//string "apple"
+//boolean true, false 
+// no var only let const'
+
+
+// let person ="ishaq";  //type fixed hgogai khud hi
+// person="56";   
+
+// let person;    //aesy type any rhygi
+// person=56;
+// person="nadir"   
+
+// let person:string = "muzammil"     //no recommended way
+
+// const names:string[]=["4","4"];
+// let names:string[]=[];
+// names.push("23");
+
+// const person:{
+//     name:string;
+//     age:number;
+//     email:string;
+//     address?:string
+// }={
+// name:"muzammil",
+// age:22,
+// email:"s.muzammiljaved@gmail.com",
+// }
+// person.age=23;
+// person.address="street no 07"
+
+// type Person={
+//     name:string;
+//     age:number;
+//     email:string;
+//     address?:string
+// }
+
+// const person1:Person={
+// name:"muzammil",
+// age:22,
+// email:"s.muzammiljaved@gmail.com",
+// }
+// person1.age=23;
+// person1.address="street no 07"
+
