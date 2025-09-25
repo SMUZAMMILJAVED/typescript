cfbf8890549daf5c8a4dc134e88225b740a3a099
S MuzammiL Javed <121771009+SMUZAMMILJAVED@users.noreply.github.com>
2025-09-25 16:28:02 +0500

    class_05

---
+++ typescript_05/src/app.ts
@@ -0,0 +1,21 @@
+                          // type casting 
+// const element=document.getElementById('input')! as HTMLInputElement;
+// // const element=<HTMLInputElement>document.getElementById('input');
+// element.value="bilal";
+
+                        //generics   (addional info  )
+// const promise:Promise<string>=new Promise((resolve,reject)=>{
+// setTimeout(()=>{
+// resolve("done");
+// },2000)
+// });
+// promise.then(data=>{
+//     data.toString()
+// });
+
+// function merge<T extends object,U extends object>(objA:T,objB:U){
+// return {...objA,...objB}
+// };
+// // console.log(merge({name:"muzammil"},{age:22}));
+// const merged=merge({name:"muzammil"},{age:22});
+// console.log(merged.age);
